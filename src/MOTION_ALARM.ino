01693f9ff534c34ded69f2c83112c9a7c565d09a
Roberto Vicario <119845903+roberrevil@users.noreply.github.com>
2023-01-23 11:50:18 +0100

    Create MOTION_ALARM.ino

---
+++ src/MOTION_ALARM.ino
@@ -0,0 +1,50 @@
+const int BUZZER_PIN = 2;
+const int LED_PIN = 3;
+const int PIR_PIN = 4;
+
+void setup()
+{
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(PIR_PIN, INPUT);
+}
+
+void loop()
+{
+  int motion = digitalRead(PIR_PIN);
+  
+  if (motion == HIGH) {
+    alarm();
+  } else {
+    digitalWrite(BUZZER_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
+  }
+}
+
+void alarm()
+{
+  digitalWrite(BUZZER_PIN, HIGH);
+  digitalWrite(LED_PIN, HIGH);
+  delay(50);
+  digitalWrite(BUZZER_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+  delay(50);
+  digitalWrite(BUZZER_PIN, HIGH);
+  digitalWrite(LED_PIN, HIGH);
+  delay(50);
+  digitalWrite(BUZZER_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+  delay(50);
+  digitalWrite(BUZZER_PIN, HIGH);
+  digitalWrite(LED_PIN, HIGH);
+  delay(50);
+  digitalWrite(BUZZER_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+  delay(50);
+  digitalWrite(BUZZER_PIN, HIGH);
+  digitalWrite(LED_PIN, HIGH);
+  delay(50);
+  digitalWrite(BUZZER_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
+  delay(50);
+}
